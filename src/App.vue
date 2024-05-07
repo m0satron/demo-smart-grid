8e0fdcc07beb23b5938175bfa16e49dd0d332313
m0satron <18460264+m0satron@users.noreply.github.com>
2024-05-07 10:13:20 +0200

    demo smart grid

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+
+
+<template>
+<div class="layout">
+
+  <div class="heading">
+    <FTitle class="title"> Smart energy communities </FTitle>
+    <FSubTitle class="subtitle"> Energy / Resource optimization </FSubTitle>
+  </div>
+  <!-- <FButton label="START DEMO"  class="button "/> -->
+  <FFooter class="footer"/>
+    <!-- <div class="wrapper bg-color-gray">
+    </div> -->
+  <AnimationContainer :animation-data="animationData" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import 'fari-component-library/style';
+import { FTitle, FSubTitle, FFooter  } from 'fari-component-library'
+import AnimationContainer from './components/AnimationContainer.vue';
+import animationData from './assets/smartgridNoTitle.json'
+</script>
+
+<style scoped lang="scss">
+.layout {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100%;
+}
+
+.heading {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space;
+  margin-top: 140px;
+}
+
+.footer {
+  margin-top: auto;
+  margin-bottom: 1.8rem;
+}
+
+.subtitle {
+  font-family: 'Rhetorik';
+}
+
+.button {
+width: 14rem;
+}
+
+</style>
---
+++ src/components/AnimationContainer.vue
@@ -0,0 +1,53 @@
+<template>
+  <div ref="lottieContainer" class="lottie-container"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import lottie from 'lottie-web';
+import type { AnimationItem } from 'lottie-web';
+
+
+const props = defineProps<{
+  animationData: Object;
+}>();
+
+const lottieContainer = ref<HTMLElement | null>(null);
+
+onMounted(() => 
+  (lottieContainer.value && props.animationData) && 
+    lottie.loadAnimation({
+      container: lottieContainer.value,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: props.animationData,
+    })
+);
+</script>
+
+<style scoped lang="scss">
+// .lottie-container {
+//   width: 1920px;
+//   height: 1080px;
+//   background-color: #f0f0f0; /* Optional: set a background color */
+//   background-image: url('@/assets/SmartEnergy_BG.svg');
+//   background-size: cover; /* Cover the entire area of the container */
+//   background-position: center; /* Center the background image */
+//   position: fixed; /* Make the container fixed to cover the exact size */
+//   left: 50%;
+//   top: 50%;
+//   transform: translate(-50%, -50%);
+// }
+
+.lottie-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 1920px;
+  height: 1080px;
+  z-index: -1; /* Ensure the animation stays in the background */
+  background-image: url('@/assets/SmartEnergy_BG.svg');  background-size: cover;
+  background-position: center;
+}
+</style>
