99833ca2cfb9f5dd7113405162487d055846cb6b
m0satron <18460264+m0satron@users.noreply.github.com>
2024-05-13 15:58:22 +0200

    feat: config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './assets/main.scss'
-
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+const pinia = createPinia()
+
+createApp(App).use(pinia).mount('#app')
