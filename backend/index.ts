7cea01bf88cb12dc6421fed00777a2c84b36bca8
m0satron <18460264+m0satron@users.noreply.github.com>
2024-05-23 11:12:54 +0200

    chore: cleanup code

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
 import cors from 'cors'; // Import cors
 import dotenv from 'dotenv';
 
@@ -11,12 +12,24 @@ const port = 3000;
 
 const strapiURL = process.env.API_URL
 
+interface Attributes {
+  locale: string;
+  logo: string
+  research_head: string
+  research_lead: string
+  explanation_short: {
+    en: string
+    nl: string
+    'fr-FR': string
+  }
+}
+
 app.get('/api/data', async (req: Request, res: Response) => {
   try {
     const response = await fetch(`${strapiURL}/api/demos/25?populate=*`);
     const {data} = await response.json();
     const { localizations, explanation_short, research_head, research_lead } = data.attributes
-    const locales = localizations.data.map(({ attributes }) => ({ locale: attributes.locale, explanation_short: attributes.explanation_short }))
+    const locales = localizations.data.map(({ attributes }: { attributes: Attributes }) => ({ locale: attributes.locale, explanation_short: attributes.explanation_short }))
 
     const airLogo = await fetch(`${strapiURL}/uploads/air_a5e1c08136.svg`);
     const logo = await airLogo.text()
