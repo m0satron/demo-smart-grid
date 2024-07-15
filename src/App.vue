d48d7e20c6888726ab4cce20f16c6f1478a4be4d
m0satron <msebdani@gmail.com>
2024-07-15 12:14:45 +0200

    feat: add google material icons

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
+  <head>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+    />
+  </head>
   <div class="layout">
     <Transition :name="transitionName">
       <StartScreen v-if="startScreenVisible" @start-demo="startDemo" />
