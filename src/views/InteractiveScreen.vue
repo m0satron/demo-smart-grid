a7e23f79bedc86765acfd45e77ec944b1d4a5d59
m0satron <msebdani@gmail.com>
2024-07-15 09:10:24 +0200

    fix: use CSV filereader composable in interactive screen

--- src/views/InteractiveScreen.vue
+++ src/views/InteractiveScreen.vue
@@ -14,7 +14,7 @@
       </template>
 
       <template #center>
-        <ToolbarValues :demand="demand" :supply="supply" />
+        <ToolbarValues :demand="currentDemand" :supply="currentSupply" />
       </template>
 
       <template #right>
@@ -24,7 +24,7 @@
               <component :is="weatherIcon" />
             </Transition>
 
-            {{ settings?.temperature }}°C
+            {{ currentTemperature }}°C
           </div>
           <div class="time-indicator">
             {{ currentTime.toLocaleTimeString('fr-BE', { hour: '2-digit', minute: '2-digit' }) }}
@@ -47,7 +47,7 @@
 <script setup lang="ts">
 import 'fari-component-library/style'
 import ToolBar from '@/components/ToolBar.vue'
-import { ref, computed, onBeforeUnmount } from 'vue'
+import { ref, computed, onBeforeUnmount, onMounted, watch } from 'vue'
 import AnimationContainer from '@/components/AnimationContainer.vue'
 import SunIcon from '@/components/icons/SunIcon.vue'
 import MoonIcon from '@/components/icons/MoonIcon.vue'
@@ -61,80 +61,93 @@ import energyMedium from '@/assets/energyAgentMedium24fpsv2.json'
 import energySlow from '@/assets/energyAgentSlow24fpsv2.json'
 import BackgroundDay from '@/components/BackgroundDay.vue'
 import BackgroundNight from '@/components/BackgroundNight.vue'
+import { useCsvData } from '@/composables/useCsvData';
 
 const settings = ref<{
   temperature: number
   dayTime: boolean
-  weather: 'sunny' | 'cloudy' | 'overcast'
+  weather: 'sunny' | 'cloudy' | 'cloudy_sunny'
+  season: 'autumn' | 'winter' | 'spring' | 'summer'
 }>({
+  season: 'autumn',
   temperature: 20,
   dayTime: true,
   weather: 'sunny'
 })
-
-const currentTime = ref(new Date('2024-01-01T13:00:00'))
-
-const updateTime = () => {
-  const now = new Date(currentTime.value.getTime() + 3600000) // Add one hour
-  currentTime.value = now
-  const hours = now.getHours()
-  settings.value.dayTime = hours >= 6 && hours < 18
-}
-
-setInterval(updateTime, 12500)
-
-onBeforeUnmount(() => clearInterval('updateTime'))
 
 const weatherIcon = computed(() => {
   if (!settings.value) return null
   if (!settings.value.dayTime) return MoonIcon
   if (settings.value.weather === 'sunny') return SunIcon
   if (settings.value.weather === 'cloudy') return CloudIcon
-  if (settings.value.weather === 'overcast') return PartCloudIcon
+  if (settings.value.weather === 'cloudy_sunny') return PartCloudIcon
   return null
 })
 
-const supply = computed(() => {
-  if (!settings.value) return 0
+const currentTime = ref(new Date('2024-01-01T13:00:00'));
+const currentTemperature = ref(settings.value.temperature);
+const currentSupply = ref(0);
+const currentDemand = ref(0)
+const supplyMoreThanDemand = ref(currentSupply.value > currentDemand.value);
+const animationData = ref(energyMedium)
 
-  const weatherValues = {
-    sunny: 98,
-    overcast: 65,
-    cloudy: 57
+const { parsedData, loadCSVFile } = useCsvData();
+
+const file = computed(() => 
+  `simulation_results_${settings.value.season || 'autumn'}_${settings.value.weather || 'sunny'}.csv`
+);
+
+const columnsToExtract = ['time', 'global_demand', 'local_supply', 'temperature'];
+
+onMounted(() => loadCSVFile(file.value, columnsToExtract));
+
+const interval = setInterval(updateTime, 2083);
+onBeforeUnmount(() => clearInterval(interval));
+
+watch(currentTime, (newTime) => {
+  const { supply, demand, temperature } = updateParsedData(newTime);
+  [currentSupply.value, currentDemand.value, currentTemperature.value] = [supply, demand, temperature];
+
+  if(supply > demand === supplyMoreThanDemand.value) return;
+
+  supplyMoreThanDemand.value = supply > demand;
+  animationData.value = updateAnimationData(supply, demand);
+});
+
+function updateTime() {
+  const now = new Date(currentTime.value.getTime() + 10 * 60 * 1000) // 10 minutes
+  
+  currentTime.value = now
+
+  if (now.getHours() === 0 && now.getMinutes() === 0) 
+    currentTime.value = new Date('2024-01-01T00:00:00')
+  
+  const hours = now.getHours()
+  settings.value.dayTime = hours >= 6 && hours < 18
+}
+
+function updateParsedData(now: Date) {
+  const time = now.toLocaleTimeString('fr-BE', { hour: '2-digit', minute: '2-digit' })
+
+  const rowData = parsedData.value.find(row => row.time === time)
+
+  const supply = Number(parseFloat(rowData?.local_supply).toFixed(3)) ?? 0
+  const demand = Number(parseFloat(rowData?.global_demand).toFixed(3)) ?? 0
+  const temperature = Number(parseInt(rowData?.temperature).toFixed(3)) ?? 0
+
+  return {
+    supply,
+    demand,
+    temperature,
   }
+}
 
-  const dayTimeValues = {
-    true: 100,
-    false: 4
-  }
-  return (weatherValues[settings.value.weather] * dayTimeValues[`${settings.value.dayTime}`]) / 10
-})
-
-const demand = computed(() => {
-  if (!settings.value) return 0
-  const weatherValues = {
-    sunny: 100,
-    overcast: 70,
-    cloudy: 60
-  }
-
-  const dayTimeValues = {
-    true: 100,
-    false: 4
-  }
-  return Math.round(
-    (weatherValues[settings.value?.weather] *
-      dayTimeValues[`${settings.value?.dayTime}`] *
-      (settings.value?.temperature / 11)) /
-      10
-  )
-})
-
-const animationData = computed(() => {
-  if (supply.value > demand.value) return energyFast
-  if (supply.value < demand.value) return energySlow
+function updateAnimationData(supply:number, demand:number) {
+  if (supply > demand) return energyFast
+  if (supply < demand) return energySlow
   return energyMedium
-})
+}
+
 </script>
 
 <style scoped lang="scss">
